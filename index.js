7c53138e8997bf9fae634eee6d3ecd57ed248bc5
learkonas <leo.nasskau@gmail.com>
2022-10-10 00:25:25 +0100

    added details on multi-select error Oct 9th

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function addItem(tweet, tag1, tag2, source, url, type, length) {
             "name": tag1
           }
         }, 
-        /*"Tags": {             //Multi-select doesn't work: code: 'validation_error',  message: 'body failed validation. Fix one:\n' +
+        /*"Tags": {             //Multi-select doesn't work: code. Error: 'validation_error',  message: 'body failed validation. Fix one:\n' + .... (full error here: https://prnt.sc/ehwby0K-kUZV)
           "object": "property_item",
           "type": "multi_select",
           "multi-select": [
