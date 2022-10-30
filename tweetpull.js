c4d31c7adb8cbbcbcb037e2dcc66c4c41c9e84ef
learkonas <leo.nasskau@gmail.com>
2022-10-30 16:23:03 +0000

    tidied the code a bit

--- tweetpull.js
+++ tweetpull.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-d
 dotenv.config();
 import { addItem } from "./index.js"; // bringing over the code from index.js, which is what adds pages to the Notion DB
 
-const TWEET_ID = "1544723110854856704";  // this is the crucial tweet ID that determines everything else
+const TWEET_ID = "1586747616049729536";  // this is the crucial tweet ID that determines everything else
 
 var tweet_author_username = "tweet_author_username";           // defining global variables
 var twitter_url = "https://twitter.com/"
@@ -82,12 +82,20 @@ async function findTweetbyID(tweetID) {                                  // defi
     tweet_author_id = tweet_response_original.data[0].author_id;                          //pulling the author's User ID
     tweet_replying_to = tweet_response_original.data[0].in_reply_to_user_id;              //pulling the User ID of the author which the saved tweet is replying to
     tweet_author_pfp_url = tweet_response_original.includes.users[0].profile_image_url;   //pulling pfp url of the author
+    tweet_address_url = tweet_address_url+TWEET_ID; // combining the generic address for tweets (twitter.com/i/status/) with the specific tweet_ID, to make twitter.com/i/status/tweet_ID
 
     // scaling up the profile image from _normal to _400x400
     if (tweet_author_pfp_url.endsWith("normal.jpg")) {                                                  // is this image 'normal'
         tweet_author_pfp_url = tweet_author_pfp_url.substring(0, tweet_author_pfp_url.length - 10);     // if so, let's change that. Get rid of the normal.jpg ending by removing the last 10 chars
         tweet_author_pfp_url = tweet_author_pfp_url.concat("400x400.jpg");                              // and make it bigger by adding 400x400.jpg to the end instead
     }
+
+    // removing reply-to tags from the start of a tweet. E.g if someone replies to a tweet, it comes up something like "@replied_to_account hello Replied To Account, I am replying to your tweet"
+    while (tweet_text.startsWith("@")) {    // trigger this while the tweet_text begins with @ 
+        let replied_to_username_length = tweet_text.indexOf(" ") + 1;  //find after how many characters the username ends, leading to a " ". If @replied_to_account, then we get 18 + 1 = 19
+        tweet_text = tweet_text.substring(replied_to_username_length); //removes the first 19 characters from the reply, leaving us with "hello Replied To Account..."       We then search to see if there is another replied-to account as well. If not, we move on
+    }    
+
 }
     
 // FIND USER BY ID
@@ -103,13 +111,7 @@ async function findUserbyID(userID) {
 
 findTweetbyID(TWEET_ID).then(() => {
     findUserbyID(tweet_author_id).then(() => {
-        tweet_address_url = tweet_address_url+TWEET_ID; // combining the generic address for tweets (twitter.com/i/status/) with the specific tweet_ID, to make twitter.com/i/status/tweet_ID
 
-        // removing reply-to tags from the start of a tweet. E.g if someone replies to a tweet, it comes up something like "@replied_to_account hello Replied To Account, I am replying to your tweet"
-        while (tweet_text.startsWith("@")) {    // trigger this while the tweet_text begins with @ 
-            let replied_to_username_length = tweet_text.indexOf(" ") + 1;  //find after how many characters the username ends, leading to a " ". If @replied_to_account, then we get 18 + 1 = 19
-            tweet_text = tweet_text.substring(replied_to_username_length); //removes the first 19 characters from the reply, leaving us with "hello Replied To Account..."       We then search to see if there is another replied-to account as well. If not, we move on
-        }    
         
         // figure out if we are dealing with a Tweet or a Thread by seeing if the saved tweet author User ID matches the User ID of the tweet being replied to. If true, it is a thread. Else (e.g. if false or null (i.e. it doesn't reply to anything)) it is a Tweet. Users have to save the bottom of a thread if they want the thread, and automatically getting any tweets above it. Even if they don't want the thread, this adds context
         
