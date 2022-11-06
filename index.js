5cc566326663ecbd0205a3a3a3ab9b685586a3b9
learkonas <leo.nasskau@gmail.com>
2022-11-06 18:26:59 +0000

    built acceptable thread formatting

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = process.env.DB_ID
 
 const cover_image_url_bank = [
-    "https://live.staticflickr.com/5571/14763952742_5405536208_b.jpg",  // 1
-    "https://live.staticflickr.com/3853/14751243442_f8b19e649f_b.jpg",  // 2
-    "https://live.staticflickr.com/3904/14769858383_5f22e4cfc6_b.jpg",  // 3
-    "https://live.staticflickr.com/5716/21117700952_d7733aaecf_b.jpg",  // 4
-    "https://live.staticflickr.com/5572/14749675191_df312fd06b_b.jpg",  // 5
-    "https://live.staticflickr.com/555/20386702806_85a7ff5470_b.jpg",   // 6
-    "https://live.staticflickr.com/5568/14758775716_bb3c66cc97_b.jpg",  // 7
-    "https://live.staticflickr.com/3881/14750109452_890b7989a4_b.jpg",  // 8
-    "https://live.staticflickr.com/3888/14771092033_32a8e39865_b.jpg",  // 9
-    "https://live.staticflickr.com/5643/20391509300_236c42c5c0_z.jpg",  // 10
-    "https://live.staticflickr.com/5737/19957209674_360a99dff7_b.jpg",  // 11
-    "https://live.staticflickr.com/614/19956900294_332cedbbf4_b.jpg",   // 12
-    "https://live.staticflickr.com/5805/19933533444_be3160a125_b.jpg"   // 13 
-  ]
+   "https://live.staticflickr.com/5571/14763952742_5405536208_b.jpg",  // 1
+   "https://live.staticflickr.com/3853/14751243442_f8b19e649f_b.jpg",  // 2
+   "https://live.staticflickr.com/3904/14769858383_5f22e4cfc6_b.jpg",  // 3
+   "https://live.staticflickr.com/5716/21117700952_d7733aaecf_b.jpg",  // 4
+   "https://live.staticflickr.com/5572/14749675191_df312fd06b_b.jpg",  // 5
+   "https://live.staticflickr.com/555/20386702806_85a7ff5470_b.jpg",   // 6
+   "https://live.staticflickr.com/5568/14758775716_bb3c66cc97_b.jpg",  // 7
+   "https://live.staticflickr.com/3881/14750109452_890b7989a4_b.jpg",  // 8
+   "https://live.staticflickr.com/3888/14771092033_32a8e39865_b.jpg",  // 9
+   "https://live.staticflickr.com/5643/20391509300_236c42c5c0_z.jpg",  // 10
+   "https://live.staticflickr.com/5737/19957209674_360a99dff7_b.jpg",  // 11
+   "https://live.staticflickr.com/614/19956900294_332cedbbf4_b.jpg",   // 12
+   "https://live.staticflickr.com/5805/19933533444_be3160a125_b.jpg"   // 13 
+]
   
 let cover_bank_number = Math.floor(Math.random() * 13);
 var cover_image_url = cover_image_url_bank[cover_bank_number];
@@ -121,100 +121,119 @@ export async function addTweet(tweet, tag1, tag2, source, url, type, length, twe
 }
 
 export async function addThread(finalArray, coreStats) {
-  var thread_text = [];
-  for (var i = 0; i < finalArray.length; i++) {
-    thread_text.push(finalArray[i][0])
-  }
-  try {
-    const response = await notion.pages.create({
-      "parent": {
-        "type": "database_id",
-        "database_id": databaseId
-      },
-      "icon": {
-        "type": "external",
-        "external": {
-          "url": String(coreStats[0]) //tweet_author_pfp_url, scaled to 400x400
-        }
-      },
-      "cover": {
-        "type": "external",
-        "external": {
-          "url": cover_image_url
-        },
-      },
-      properties: {
-        "Tweet": {
-          "title":[{
-            "text": {
-              "content": String(coreStats[4]) //top_line
+   try {
+      const response = await notion.pages.create({
+         "parent": {
+         "type": "database_id",
+         "database_id": databaseId
+         },
+         "icon": {
+            "type": "external",
+            "external": {
+               "url": String(coreStats[0]) //tweet_author_pfp_url, scaled to 400x400
             }
-          }]
-        },
-        "Source": {
-          "rich_text": [{
-            "text": {
-              "content": String(coreStats[3]), // author_name
-              "link": {
-                "url": String(finalArray[0][4]) //tweet_address_url
-              }
+         },
+         "cover": {
+            "type": "external",
+            "external": {
+               "url": cover_image_url
             },
-            "href": String(finalArray[0][4]) //tweet_address_url
-          }]
-        },
-        "Type": {
-          "select": {
-            "name": "Thread"
-          }
-        },
-        "Length": {
-          "number": finalArray.length
-        },
-        "Tweeted": {
-          "date": {
-            "start": String(finalArray[0][1])
-          }
-        },
-        "Tags": { 
-          "multi_select": [
-            {
-              "name": "Tag1"
-            },
-            {
-              "name": "Tag2"
-            }
-          ]
-        },
-      },
-      "children": [
-        {
-          "object": "block",
-            "heading_3": {
-              "rich_text": [{
-                "text": {
-                  "content": String(coreStats[4]) //top_line
-                }
-              }]
-            }
-        },
-        {
-          "object": "block",
-            "paragraph": {
-              "rich_text": [
-                {
+         },
+         properties: {
+            "Tweet": {
+               "title":[{
                   "text": {
-                    "content": String(thread_text)
-                  },
-                }
-              ],
-              "color": "default"
+                     "content": String(coreStats[4]) //top_line
+                  }
+               }]
+            },
+            "Source": {
+            "rich_text": [{
+               "text": {
+                  "content": String(coreStats[3]), // author_name
+                  "link": {
+                     "url": String(finalArray[0][4]) //tweet_address_url
+                  }
+               },
+               "href": String(finalArray[0][4]) //tweet_address_url
+            }]
+            },
+            "Type": {
+               "select": {
+                  "name": "Thread"
+               }
+            },
+            "Length": {
+               "number": finalArray.length
+            },
+            "Tweeted": {
+               "date": {
+               "start": String(finalArray[0][1])
+               }
+            },
+            "Tags": { 
+               "multi_select": [
+                  { "name": "Tag1" },
+                  { "name": "Tag2" }
+               ]
+            },
+         },
+         "children": [{
+            "object": "block",
+            "heading_3": {
+               "rich_text": [{
+                  "text": {
+                     "content": String(coreStats[4]) //top_line
+                  }
+               }]
             }
-        }
-      ]
-    })
-    //console.log(response)
-    console.log("Success! Thread added.")
-  } catch (e) {
-    console.error(e.body)
-  }
+         }]
+      })
+      //console.log(response)
+      const blockId = response["id"]   
+      for (var i = 0; i < finalArray.length; i++) {
+         const responseTweet = await notion.blocks.children.append({
+         block_id: blockId,
+            children: [
+               /*{
+                  "paragraph": {
+                     "rich_text": [{
+                        "type": "text",
+                        "text": {
+                           "content": String(finalArray[i][0]) + "\n \n" + '<a href=\"http://google.com/\">Google</a>',
+                        }
+                  }],
+               }
+               },         */        
+               {
+                  "embed": {
+                     "url": String(finalArray[i][4])
+                  }
+               }
+            ],
+         });
+         var top_link = String(finalArray[0][4])
+      }
+      const responseClosing = await notion.blocks.children.append({
+         block_id: blockId,
+         children: [
+            {
+               "paragraph": {
+                  "rich_text": [{
+                     "text": {
+                        "content": "Click to read on Twitter",
+                        "link": {
+                           "url": top_link
+                        }
+                     }
+                  }]
+               }
+            }
+         ]
+      })
+      console.log("Success! Thread added.")
+   }
+   catch (e) {
+      console.error(e.body)
+   }
 }
--- tweetpull.js
+++ tweetpull.js
@@ -4,7 +4,7 @@ dotenv.config();
 import { addTweet } from "./index.js"; // bringing over the code from index.js, which is what adds pages to the Notion DB
 import { addThread } from "./index.js"; // bringing over the code from index.js, which is what adds pages to the Notion DB
 
-const TWEET_ID = "1586294774587305984";  // this is the crucial tweet ID that determines everything else
+const TWEET_ID = "1575762790325047298";  // this is the crucial tweet ID that determines everything else
 var finalArray = []; // this will be the final array of all the tweets etc that we use to populate a page in the Notion DB
 var coreStats = [];  // this will be the final array of the core data we add into that Notion DB page
 
@@ -31,136 +31,128 @@ var length = 0;
 
 // FIND TWEET BY ID
 async function findTweetbyID(tweetID) {                                  // defining find Tweet by ID function
-    const tweet_response_current = await client.tweets.findTweetsById({
-        "ids": [
-            tweetID
-        ],
+   const tweet_response_current = await client.tweets.findTweetsById({
+      "ids": [
+         tweetID
+      ],
+      "tweet.fields": [
+         "author_id",
+         "created_at",
+         "id",
+         "in_reply_to_user_id",
+         "possibly_sensitive",
+         "text",
+         "withheld"
+      ],
+      "expansions": [
+         "attachments.media_keys",
+         "attachments.poll_ids",
+         "author_id",
+         "entities.mentions.username",
+         "in_reply_to_user_id",
+         "referenced_tweets.id",
+         "referenced_tweets.id.author_id"
+      ],
+      "media.fields": [
+         "height",
+         "media_key",
+         "preview_image_url",
+         "type",
+         "url",
+         "width"
+      ],
+      "poll.fields": [
+         "end_datetime",
+         "id",
+         "options",
+         "voting_status"
+      ],
+      "user.fields": [
+         "id",
+         "name",
+         "profile_image_url",  // this produces a link in the form: https://pbs.twimg.com/profile_images/19_DIGIT_NUMBER/8_(NON)CAPS_LETTERS_normal.jpg but I would like it to be this, but ending _400x400.jpg... might not be worth the effort
+         "protected",
+         "url",
+         "username",
+         "withheld"
+      ]
+   });    //querying the Twitter API for the tweet by the tweet ID
+   //console.log(tweet_response_current);                                               //print response
+   //console.log(JSON.stringify(tweet_response_current, null, 2))                       //print response in string form
+   tweet_text = tweet_response_current.data[0].text;                                    //pulling the text of the saved tweet
+   tweet_created = tweet_response_current.data[0].created_at;                           //pulling when the saved tweet was written, something like 2022-10-03T11:24:34.000Z
+   tweet_created = tweet_created.substring(0, tweet_created.length - 14);       //adjust the created date to just the date w/o the time by removing the last 14 chars
+   tweet_author_id = tweet_response_current.data[0].author_id;                          //pulling the author's User ID
+   tweet_replying_to_author = tweet_response_current.data[0].in_reply_to_user_id;       //pulling the User ID  to which the saved tweet is replying to
+   try { replied_to_tweet_ID = tweet_response_current.data[0].referenced_tweets[0].id; } catch {} // pulling the Tweet ID to which the saved tweet is replying to
+   let tweet_address_url = tweet_address_url_base+tweetID;                            // combining the generic address for tweets (twitter.com/i/status/) with the specific tweet_ID, to make twitter.com/i/status/tweet_ID
+   tweet_author_pfp_url = tweet_response_current.includes.users[0].profile_image_url;   //pulling pfp url of the author
 
-        "tweet.fields": [
-            "author_id",
-            "created_at",
-            "id",
-            "in_reply_to_user_id",
-            "possibly_sensitive",
-            "text",
-            "withheld"
-        ],
-
-        "expansions": [
-            "attachments.media_keys",
-            "attachments.poll_ids",
-            "author_id",
-            "entities.mentions.username",
-            "in_reply_to_user_id",
-            "referenced_tweets.id",
-            "referenced_tweets.id.author_id"
-        ],
-
-        "media.fields": [
-            "height",
-            "media_key",
-            "preview_image_url",
-            "type",
-            "url",
-            "width"
-        ],
-        "poll.fields": [
-            "end_datetime",
-            "id",
-            "options",
-            "voting_status"
-        ],
-        "user.fields": [
-            "id",
-            "name",
-            "profile_image_url",  // this produces a link in the form: https://pbs.twimg.com/profile_images/19_DIGIT_NUMBER/8_(NON)CAPS_LETTERS_normal.jpg but I would like it to be this, but ending _400x400.jpg... might not be worth the effort
-            "protected",
-            "url",
-            "username",
-            "withheld"
-        ]
-    });    //querying the Twitter API for the tweet by the tweet ID
-    //console.log(tweet_response_current);                                               //print response
-    //console.log(JSON.stringify(tweet_response_current, null, 2))                       //print response in string form
-    tweet_text = tweet_response_current.data[0].text;                                    //pulling the text of the saved tweet
-    tweet_created = tweet_response_current.data[0].created_at;                           //pulling when the saved tweet was written, something like 2022-10-03T11:24:34.000Z
-    tweet_created = tweet_created.substring(0, tweet_created.length - 14);       //adjust the created date to just the date w/o the time by removing the last 14 chars
-    tweet_author_id = tweet_response_current.data[0].author_id;                          //pulling the author's User ID
-    tweet_replying_to_author = tweet_response_current.data[0].in_reply_to_user_id;       //pulling the User ID  to which the saved tweet is replying to
-    replied_to_tweet_ID = tweet_response_current.data[0].referenced_tweets[0].id;    //pulling the Tweet ID to which the saved tweet is replying to
-    let tweet_address_url = tweet_address_url_base+TWEET_ID;                            // combining the generic address for tweets (twitter.com/i/status/) with the specific tweet_ID, to make twitter.com/i/status/tweet_ID
-    tweet_author_pfp_url = tweet_response_current.includes.users[0].profile_image_url;   //pulling pfp url of the author
-
-    // removing reply-to tags from the start of a tweet. E.g if someone replies to a tweet, it comes up something like "@replied_to_account hello Replied To Account, I am replying to your tweet"
-    while (tweet_text.startsWith("@")) {    // trigger this while the tweet_text begins with @ 
-        let replied_to_username_length = tweet_text.indexOf(" ") + 1;  //find after how many characters the username ends, leading to a " ". If @replied_to_account, then we get 18 + 1 = 19
-        tweet_text = tweet_text.substring(replied_to_username_length); //removes the first 19 characters from the reply, leaving us with "hello Replied To Account..."       We then search to see if there is another replied-to account as well. If not, we move on
-    }
-
-    let baseArray = [[tweet_text],[tweet_created],
+   // removing reply-to tags from the start of a tweet. E.g if someone replies to a tweet, it comes up something like "@replied_to_account hello Replied To Account, I am replying to your tweet"
+   while (tweet_text.startsWith("@")) {    // trigger this while the tweet_text begins with @ 
+      let replied_to_username_length = tweet_text.indexOf(" ") + 1;  //find after how many characters the username ends, leading to a " ". If @replied_to_account, then we get 18 + 1 = 19
+      tweet_text = tweet_text.substring(replied_to_username_length); //removes the first 19 characters from the reply, leaving us with "hello Replied To Account..."       We then search to see if there is another replied-to account as well. If not, we move on
+   }     //it also removes when tweets start with an @, which is an edge case
+   let baseArray = [[tweet_text],[tweet_created],
                     [tweet_author_id], [tweet_replying_to_author],
                     [tweet_address_url],
                     [TWEET_ID], [replied_to_tweet_ID],];
-    finalArray.push(baseArray);
-    length = finalArray.length;
-    if (length == 1) {
-        originalTweet = baseArray;
-    }
+   finalArray.push(baseArray);
+   length = finalArray.length;
+   if (length == 1) {
+      originalTweet = baseArray;
+   }
     
-    if (Number(baseArray[2]) == Number(baseArray[3])) {     //if the tweet author ID matches the ID of the tweet author above it, then...
-        tweetID = replied_to_tweet_ID       //take the ID of the tweet above it and make that the tweetID
-        findTweetbyID(tweetID);
-    }
-    else {  
-        finalArray.reverse(finalArray);
+   if (Number(baseArray[2]) == Number(baseArray[3])) {     //if the tweet author ID matches the ID of the tweet author above it, then...
+      tweetID = replied_to_tweet_ID       //take the ID of the tweet above it and make that the tweetID
+      findTweetbyID(tweetID);
+   }
+   else {  
+      finalArray.reverse(finalArray);
 
-        let top_line_ends = String(finalArray[0][0]).indexOf("\n");    //cleaning up the title for the page on Notion
-        console.log(top_line_ends)
-        if (top_line_ends < 0) {                
-            top_line = String(finalArray[0][0]).substring(0, 35); 
-            top_line = top_line+"..." 
-        }
-        else if (top_line_ends < 40){
-            top_line = String(finalArray[0][0]).substring(0, top_line_ends);
-        }
-        else {          
-            top_line = String(finalArray[0][0]).substring(0, 50); 
-            top_line = top_line+"..."
-        }
+      let top_line_ends = String(finalArray[0][0]).indexOf("\n");    //cleaning up the title for the page on Notion
+      if (top_line_ends < 0) {                
+         top_line = String(finalArray[0][0]).substring(0, 32); 
+         top_line = top_line+"..." 
+      }
+      else if (top_line_ends < 35){
+         top_line = String(finalArray[0][0]).substring(0, top_line_ends);
+      }
+      else {          
+         top_line = String(finalArray[0][0]).substring(0, 45); 
+         top_line = top_line+"..."
+      }
 
-        if (length == 1) {
-            tweet_thread_status = "Tweet";
-            //console.log("There is", String(length), "tweet.")
-            findUserbyID(tweet_author_id).then(() => {
+      if (length == 1) {
+         tweet_thread_status = "Tweet";
+         //console.log("There is", String(length), "tweet.")
+         findUserbyID(tweet_author_id).then(() => {
             addTweet(String(originalTweet[0]), "Tag1", "Tag2", String(coreStats[3]), String(originalTweet[4]), tweet_thread_status, length, String(originalTweet[1]), String(coreStats[0]), top_line); //add the item, here an individual tweet, to Notion DB   
-            })
-            return;
-        }
-        else {
-            tweet_thread_status = "Thread";
-            //console.log("There are", String(length), "tweets.")  
-            findUserbyID(tweet_author_id).then(() => {  
+         })
+         return;
+      }
+      else {
+         tweet_thread_status = "Thread";
+         //console.log("There are", String(length), "tweets.")  
+         findUserbyID(tweet_author_id).then(() => {  
             addThread(finalArray, coreStats); //add the item, here a multi-tweet thread, to Notion DB   
-            })
-            return;
-        }
-    }
+         })
+         return;
+      }
+   }
 }
-
 // FIND USER BY ID
 async function findUserbyID(userID) {
-    // scaling up the profile image from _normal to _400x400
-    if (tweet_author_pfp_url.endsWith("normal.jpg")) {                                                  // is this image 'normal'
-        tweet_author_pfp_url = tweet_author_pfp_url.substring(0, tweet_author_pfp_url.length - 10);     // if so, let's change that. Get rid of the normal.jpg ending by removing the last 10 chars
-        tweet_author_pfp_url = tweet_author_pfp_url.concat("400x400.jpg");                            // and make it bigger by adding 400x400.jpg to the end instead
-    }
-
-    const user_response = await client.users.findUserById(userID,);  // calling the Twitter API for User with the User ID
-    author_name = user_response.data.name;                 //  getting the Name   of the author of the saved tweet
-    tweet_author_username = user_response.data.username;   //  getting the Handle of the author of the saved tweet
-    tweet_author_url = twitter_url+tweet_author_username   //  combining the generic twitter url (twitter.com/) with the author's specific handle tweet_author_url to produce twitter.com/tweet_author_url
-    coreStats = [[tweet_author_pfp_url], [tweet_author_id],
+   // scaling up the profile image from _normal to _400x400
+   if (tweet_author_pfp_url.endsWith("normal.jpg")) {                                                  // is this image 'normal'
+      tweet_author_pfp_url = tweet_author_pfp_url.substring(0, tweet_author_pfp_url.length - 10);     // if so, let's change that. Get rid of the normal.jpg ending by removing the last 10 chars
+      tweet_author_pfp_url = tweet_author_pfp_url.concat("400x400.jpg");                            // and make it bigger by adding 400x400.jpg to the end instead
+   }
+   const user_response = await client.users.findUserById(userID,);  // calling the Twitter API for User with the User ID
+   author_name = user_response.data.name;                 //  getting the Name   of the author of the saved tweet
+   tweet_author_username = user_response.data.username;   //  getting the Handle of the author of the saved tweet
+   tweet_author_url = twitter_url+tweet_author_username   //  combining the generic twitter url (twitter.com/) with the author's specific handle tweet_author_url to produce twitter.com/tweet_author_url
+   coreStats = [[tweet_author_pfp_url], [tweet_author_id],
                 [tweet_author_url], [author_name], [top_line]];
 }
-
 findTweetbyID(TWEET_ID);
