c1b811bd37f9eab17d77f16602a01206cd0e9011
learkonas <leo.nasskau@gmail.com>
2022-12-10 15:06:38 +0000

    restoring to boilerplate vars

--- example_scripts/webhook_management/update-wehbhook-url.js
+++ example_scripts/webhook_management/update-wehbhook-url.js
@@ -23,7 +23,7 @@ var twitter_oauth = {
   token_secret: nconf.get('TWITTER_ACCESS_TOKEN_SECRET')
 }
 
-var WEBHOOK_URL = 'https://your-webhook-url'
+var WEBHOOK_URL = 'https://eox20rp6uzpjk00.m.pipedream.net'
 
 
 /**
